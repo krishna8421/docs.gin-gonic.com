62c8b7a66c31cd50ef228a8671d568b738ca6050
Krishna Kumar <dev@krishnaaa.com>
2023-05-28 06:10:59 +0530

    Introduction and Installation

---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Gin Gonic</span>,
+  project: {
+    link: 'https://github.com/krishna8421/docs.gin-gonic.com',
+  },
+  docsRepositoryBase: 'https://github.com/krishna8421/docs.gin-gonic.com',
+  footer: {
+    text: "Gin Gonic Docs © 2023",
+  },
+}
+
+export default config
